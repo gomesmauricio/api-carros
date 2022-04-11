981b356612f359efc3e3028acce92e27f19876db
Mauricio <Tit@m125>
2022-04-11 11:21:51 -0300

    Refatorando pacotes

---
+++ src/main/java/com/example/carros/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.example.carros.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String message){
+	        super(message);
+
+	    }
+
+}
---
+++ src/main/java/com/example/carros/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.example.carros.security;
+
+import com.example.carros.domain.model.User;
+import com.example.carros.domain.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service(value = "userDetailsService")
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+        User user = userRepository.findByLogin(username);
+        if (user == null){
+            throw new UsernameNotFoundException("user not found");
+        }
+
+        return user;
+
+    }
+}

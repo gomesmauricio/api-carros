583532e75d9300b7656952a4dae4d0529998dd07
Mauricio <Tit@m125>
2022-04-11 13:41:26 -0300

    Adicionando jwt

--- src/main/java/com/example/carros/security/SecurityConfig.java
+++ src/main/java/com/example/carros/security/SecurityConfig.java
@@ -1,8 +1,14 @@
 package com.example.carros.security;
 
+import com.example.carros.security.jwt.JwtAuthenticationFilter;
+import com.example.carros.security.jwt.JwtAuthorizationFilter;
+import com.example.carros.security.jwt.handler.AccessDeniedHandler;
+import com.example.carros.security.jwt.handler.UnauthorizedHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,20 +24,45 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
+    private AccessDeniedHandler accessDeniedHandler;
+
+    @Autowired
+    private UnauthorizedHandler unauthorizedHandler;
+
+    @Autowired
     @Qualifier("userDetailsService")
     private UserDetailsService userDetailsService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        AuthenticationManager authManager = authenticationManager();
+
+        http
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .antMatchers(HttpMethod.GET, "/api/v1/login").permitAll()
+                .and().csrf().disable()
+                .addFilter(new JwtAuthenticationFilter(authManager))
+                .addFilter(new JwtAuthorizationFilter(authManager, userDetailsService))
+                .exceptionHandling()
+                .accessDeniedHandler(accessDeniedHandler)
+                .authenticationEntryPoint(unauthorizedHandler)
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+
+        /*   Segurança com HTTP Basic
         http
                 .authorizeRequests().antMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .httpBasic()
                 .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//não cria cook de cessao
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)//deliga acriação de cook de cessao
                 .and()
                 .csrf().disable();
+        */
 
     }
 
